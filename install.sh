c830fad7d380904a3981bff2cf3e5deade2c485e
Ammar Faizi <ammarfaizi2@gmail.com>
2018-01-07 06:51:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ php --version >> /dev/null 2>&1 ||
 
 python --version >> /dev/null 2>&1 ||
 (echo "Python is not installed" && exit 1)
-
-echo "Ready!"
+(cp $(php -r "print __DIR__.\"/bin/youtube-dl\";") /tmp/youtube-dl && chmod +rwx /tmp/youtube-dl) ||
+echo "Failed!" &&
+echo "Ready!" 
 exit 0
